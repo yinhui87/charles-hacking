dae810898d380492fd968cba4ebad8d78ab76239
kuiki <911yinhui911@163.com>
2017-09-05 09:03:48 +0800

    add charles.4.1.4.jar, rename user name

--- hack.4.1.4.sh
+++ hack.4.1.4.sh
@@ -7,7 +7,7 @@ cat >> gIbD.java <<EOF
 package com.xk72.charles;
 public final class gIbD {
 	public static boolean xUFT() { return true; }
-	public static String PcqR() { return "Cracked"; }
+	public static String PcqR() { return "Kuiki"; }
 	public static String xUFT(String name, String key) { return null; }
 }
 EOF
